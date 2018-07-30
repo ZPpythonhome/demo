9672ede35b35929e3afa6f96a0a5a4f8e90f2816
ZPpythonhome <1095622913@qq.com>
2018-07-30 21:12:15 +0800

    second commit

--- post/models.py
+++ post/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length=64)
     created = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
+class Apple(models.Model):
+	pass
